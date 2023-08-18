55e0b9ddc607aef06902a6e8343327827dd6867d
Devarshi <devarshidwi@gmail.com>
2023-08-18 22:02:58 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,76 @@
+const session = document.getElementById('session');
+const courseType = document.getElementById('courseType');
+const examType = document.getElementById('examType');
+const course = document.getElementById('course');
+const year = document.getElementById('year');
+const rollNo = document.getElementById('rollNo');
+const submitBtn = document.querySelector('.submitBtn');
+const mainLink = document.getElementById('mainLink');
+
+const arr = [session, courseType, examType, course, year];
+arr.forEach((element) => {
+	element.addEventListener('change', () => {
+		if (everythingFilled() && everythingFilledCorrectly()) {
+			mainLink.setAttribute('href', './assets/result.pdf');
+			mainLink.setAttribute('download', 'result');
+		}
+	});
+});
+
+rollNo.addEventListener('keyup', () => {
+	if (everythingFilled() && everythingFilledCorrectly()) {
+		mainLink.setAttribute('href', './assets/result.pdf');
+		mainLink.setAttribute('download', 'result');
+	}
+});
+
+submitBtn.addEventListener('click', () => {
+	validator();
+});
+
+function validator() {
+	if (everythingFilled() && everythingFilledCorrectly()) {
+		mainLink.setAttribute('href', './assets/result.pdf');
+		mainLink.setAttribute('download', 'result');
+	} else if (everythingFilled()) {
+		mainLink.setAttribute('href', '.#');
+		mainLink.removeAttribute('download');
+		alert('Wrong Roll No.');
+	} else {
+		mainLink.setAttribute('href', '.#');
+		mainLink.removeAttribute('download');
+		alert('Fill All the values');
+	}
+}
+
+function everythingFilled() {
+	if (
+		session.value !== 'nil' &&
+		courseType.value !== 'nil' &&
+		examType.value !== 'nil' &&
+		course.value !== 'nil' &&
+		year.value !== 'nil' &&
+		rollNo.value !== ''
+	) {
+		console.log('Everything is filled');
+		return true;
+	}
+	console.log('Everything is not filled');
+	return false;
+}
+
+function everythingFilledCorrectly() {
+	if (
+		session.value === 'correct' &&
+		courseType.value === 'correct' &&
+		examType.value === 'correct' &&
+		course.value === 'correct' &&
+		year.value === 'correct' &&
+		rollNo.value === '0325877'
+	) {
+		console.log('Everything is filled correctly');
+		return true;
+	}
+	console.log('Everything is not filled correctly');
+	return false;
+}
